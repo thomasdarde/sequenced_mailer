d810d60edca2ea969f0d6d59c6c4c706eb8df0d0
Thomas Darde <thomas@rougecardinal.fr>
2023-06-20 22:33:21 +0200

    Initial commmit

---
+++ lib/sequenced_mailer.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "sequenced_mailer/version"
+
+module SequencedMailer
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/sequenced_mailer/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module SequencedMailer
+  VERSION = "0.1.0"
+end
