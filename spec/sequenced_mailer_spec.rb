77809b71ab5dc5ce4d54a3ead48cfbdb889ad32c
Thomas Darde <thomas@rougecardinal.fr>
2023-10-18 10:29:26 +0200

    remove a spec, specs are not ready

--- spec/sequenced_mailer_spec.rb
+++ spec/sequenced_mailer_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe SequencedMailer do
   it "has a version number" do
     expect(SequencedMailer::VERSION).not_to be_nil
   end
-
-  it "does something useful" do
-    expect(false).to be(true)
-  end
 end
